28fea47668d4b4d3423d0e93005c66585745a9fd
Rachyl <rachylquan@yahoo.com>
2020-08-24 12:09:35 -0700

    added users and comments

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const xss = require('xss')
 const ArticlesService = require('./articles-service')
@@ -11,6 +12,7 @@ const serializeArticle = article => ({
   title: xss(article.title),
   content: xss(article.content),
   date_published: article.date_published,
+  author: article.author,
 })
 
 articlesRouter
@@ -24,7 +26,7 @@ articlesRouter
       .catch(next)
   })
   .post(jsonParser, (req, res, next) => {
-    const { title, content, style } = req.body
+    const { title, content, style, author } = req.body
     const newArticle = { title, content, style }
 
     for (const [key, value] of Object.entries(newArticle))
@@ -33,6 +35,8 @@ articlesRouter
           error: { message: `Missing '${key}' in request body` }
         })
 
+    newArticle.author = author
+
     ArticlesService.insertArticle(
       req.app.get('db'),
       newArticle
@@ -40,7 +44,7 @@ articlesRouter
       .then(article => {
         res
           .status(201)
-          .location(`/articles/${article.id}`)
+          .location(path.posix.join(req.originalUrl, `/${article.id}`))
           .json(serializeArticle(article))
       })
       .catch(next)
@@ -48,16 +52,54 @@ articlesRouter
 
 articlesRouter
   .route('/:article_id')
-  .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
+  .all((req, res, next) => {
+    ArticlesService.getById(
+      req.app.get('db'),
+      req.params.article_id
+    )
       .then(article => {
         if (!article) {
           return res.status(404).json({
             error: { message: `Article doesn't exist` }
           })
         }
-        res.json(serializeArticle(article))
+        res.article = article
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
+  })
+  .delete((req, res, next) => {
+    ArticlesService.deleteArticle(
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const articleToUpdate = { title, content, style }
+
+    const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'title', 'style' or 'content'`
+        }
+      })
+
+    ArticlesService.updateArticle(
+      req.app.get('db'),
+      req.params.article_id,
+      articleToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
       })
       .catch(next)
   })
